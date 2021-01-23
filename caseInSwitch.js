6b9d5febd85c4bc454f8af0be7e10f09e6e7b9fa
Jasmine Anderson <jazz2900@gmail.com>
2021-01-23 15:54:35 -0500

    added examples of sequential cases

--- caseInSwitch.js
+++ caseInSwitch.js
@@ -28,7 +28,34 @@ function caseInSwitch(val) {
     return answer;
 }
 
-// change the val to test if function is operating corecctly
-console.log(caseInSwitch(1));
+function sequentialCases(val) {
+    var answer = "";
+
+    switch(val) {
+        // for muliple cases with same return value in a row
+        // declare return value at last case that returns that value
+        case 1:
+        case 2:
+        case 3:
+            answer = "same"
+            // have to add break if now case(1,2,3) will disregard "same"
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "sameAgain"
+            break;
+    }
+
+    return answer;
+}
+
+
+// change the val to test if function is operating corectly
+console.log("caseInSwitch(1):\n\t" + caseInSwitch(1));
+
+console.log("sequentialCases(2):\n\t" + sequentialCases(2));
+console.log("sequentialCases(4):\n\t" + sequentialCases(4));
+
 
 // To run from commandline: node caseInSwitch.js
