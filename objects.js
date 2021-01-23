827ebd1e119207195bb53985c47a7f305d6a68cd
Jasmine Anderson <jazz2900@gmail.com>
2021-01-23 16:45:50 -0500

    accessing and manipulating objects

--- objects.js
+++ objects.js
@@ -15,3 +15,61 @@ var myDog = {
     "tails": 2,
     "friends": []
 };
+
+// dot notation to access values
+var nameValue = myDog.name;
+console.log("myDog nameValue:\n\t" + nameValue)
+
+// bracket notation to access values
+var friendsValue = ourDog["friends"];
+console.log('ourDog friendsValue:\n\t' + friendsValue);
+
+// accessing object properties with variables
+var legsValue = myDog["legs"];
+console.log("myDog legsValue:\n\t:" + legsValue)
+
+// reassigning values
+myDog.name = "Happy Coder";
+console.log("myDog renamed to:\n\t" + myDog.name)
+
+// adding additional property to object
+myDog['bark'] = 'woof';
+console.log("new bark property:\n\t:" + myDog.bark)
+
+// deleting properties
+delete myDog.bark
+console.log("deleted bark?:\n\t" + myDog.bark)
+
+// use object property to look up value
+function phoneticLookup(val) {
+    var result = "";
+
+    var lookup = {
+        alpha: "Adams",
+        bravo: "Boston",
+        charlie: "Chicago",
+        delta: "Denver",
+        echo: "Easy",
+        foctrot: "frank"
+    };
+
+    result = lookup[val];
+
+    return result;
+
+}
+
+console.log("lookup value:\n\t" + phoneticLookup('charlie'));
+
+// testing objects for property
+function checkObj(checkProp) {
+    if (myDog.hasOwnProperty(checkProp)) {
+        return myDog[checkProp];
+    } else {
+        return "Not Found"
+    }
+}
+
+console.log("testing for object:\n\t" + checkObj('name'))
+
+
