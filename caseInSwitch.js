59bad0a473ae872438f226b7ad96249c731cd6c9
Jasmine Anderson <jazz2900@gmail.com>
2021-01-23 15:28:48 -0500

    example of switch statement

---
+++ caseInSwitch.js
@@ -0,0 +1,30 @@
+// Switch Statements
+
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val) {
+        // : is a comparison operating, "if equal"
+        case 1:
+            answer = "alpha";
+            // break means end of the case statement
+            break;
+        case 2:
+            answer = "beta";
+            break;
+
+        case 3:
+            answer = "gamma";
+            break; 
+            
+        case 4:
+            answer = "delta";
+            break;     
+    }
+
+    return answer;
+}
+
+// change the val to test if function is operating corecctly
+console.log(caseInSwitch(1));
+
+// To run from commandline: node caseInSwitch.js
