f7a75e15e38f205873a8cbaad330e16d90450c36
Jasmine Anderson <jazz2900@gmail.com>
2021-01-23 16:11:45 -0500

    object example

---
+++ objects.js
@@ -0,0 +1,17 @@
+// Objects in javascript
+
+var ourDog = {
+    // "property" : "value"
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything"]
+};
+
+
+var myDog = {
+    "name": "Quincy",
+    "legs": 3,
+    "tails": 2,
+    "friends": []
+};
